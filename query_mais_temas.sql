3a3f7c35b162dafc2a68608477654064d594dd6d
andreeigenheer <81515554+andreeigenheer@users.noreply.github.com>
2021-06-22 16:40:54 -0300

    Delete query_mais_temas.sql

--- query_mais_temas.sql
+++
@@ -1,9 +0,0 @@
-#Quais temas tem mais sets lançados?
-
-SELECT themes.name, COUNT(sets.theme_id) AS soma
-FROM sets INNER JOIN themes 
-ON sets.theme_id = themes.id
-GROUP BY themes.name
-ORDER BY soma DESC
-LIMIT 10;
-
