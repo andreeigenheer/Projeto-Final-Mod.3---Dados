b9084dde14beb0fa643763e6cff6bd98712a597e
andreeigenheer <81515554+andreeigenheer@users.noreply.github.com>
2021-06-22 16:40:26 -0300

    Delete query_cores_utilizadas.sql

--- query_cores_utilizadas.sql
+++
@@ -1,7 +0,0 @@
-#Quais as cores mais utilizadas nas peças LEGO?
-
-SELECT colors.name, COUNT(inventory_parts.color_id) 
-FROM inventory_parts
-JOIN colors ON inventory_parts.color_id = colors.id
-GROUP BY colors.name
-ORDER BY COUNT(inventory_parts.color_id) DESC LIMIT 10;
